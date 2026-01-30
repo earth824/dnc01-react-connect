e92a023dfaceb5c987f8af1f55e9cd6f0609ded9
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-01-30 14:52:21 +0700

    validate register input

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,6 @@
+import z from 'zod';
 import { axios } from '../config/axios';
+import { useState } from 'react';
 
 // export default function RegisterPage() {
 //   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
@@ -36,11 +38,59 @@ import { axios } from '../config/axios';
 //   );
 // }
 
+const registerSchema = z.object({
+  firstName: z.string().min(1, 'first name cannot be empty'),
+  lastName: z.string().min(1, 'last name cannot be empty'),
+  email: z.email('invalid email address'),
+  password: z
+    .string()
+    .regex(
+      /^[a-zA-Z0-9]{6,}$/,
+      'password must have at least 6 characters and have only letter and number'
+    )
+});
+
+type RegisterInput = z.infer<typeof registerSchema>;
+
 export default function RegisterPage() {
+  const [input, setInput] = useState<RegisterInput>({
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: ''
+  });
+
+  const [inputError, setInputError] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: ''
+  });
+
+  const handleChangeInput = (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>
+  ) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const res = await axios.post('/auth/register');
-    console.log(res);
+    const { success, data, error } = registerSchema.safeParse(input);
+    if (!success) {
+      const nextError: Partial<RegisterInput> = {};
+      for (const key in z.flattenError(error).fieldErrors) {
+        const value =
+          z.flattenError(error).fieldErrors[
+            key as keyof Partial<RegisterInput>
+          ];
+        if (value) {
+          nextError[key as keyof Partial<RegisterInput>] = value[0];
+        }
+      }
+      setInputError({ ...inputError, ...nextError });
+    }
+    // const res = await axios.post('/auth/register');
+    // console.log(res);
   };
 
   return (
@@ -48,22 +98,46 @@ export default function RegisterPage() {
       <form className="grid gap-6" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="">First Name</label>
-          <input type="text" className="border px-3 py-1.5 w-full" />
+          <input
+            type="text"
+            className="border px-3 py-1.5 w-full"
+            onChange={handleChangeInput}
+            value={input.firstName}
+            name="firstName"
+          />
         </div>
 
         <div>
           <label htmlFor="">Last Name</label>
-          <input type="text" className="border px-3 py-1.5 w-full" />
+          <input
+            type="text"
+            className="border px-3 py-1.5 w-full"
+            onChange={handleChangeInput}
+            value={input.lastName}
+            name="lastName"
+          />
         </div>
 
         <div>
           <label htmlFor="">Email</label>
-          <input type="text" className="border px-3 py-1.5 w-full" />
+          <input
+            type="text"
+            className="border px-3 py-1.5 w-full"
+            onChange={handleChangeInput}
+            value={input.email}
+            name="email"
+          />
         </div>
 
         <div>
           <label htmlFor="">Password</label>
-          <input type="text" className="border px-3 py-1.5 w-full" />
+          <input
+            type="password"
+            className="border px-3 py-1.5 w-full"
+            onChange={handleChangeInput}
+            value={input.password}
+            name="password"
+          />
         </div>
 
         <div>
