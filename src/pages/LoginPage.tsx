d959cfba70249befc24fa210252ae9842e9795aa
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-02 11:41:55 +0700

    add login validation and cal login api

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,62 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import z from 'zod';
+import { axios } from '../config/axios';
+import { toast } from 'sonner';
+import { AxiosError } from 'axios';
+
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6)
+});
+
+type LoginInput = z.infer<typeof loginSchema>;
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginInput>({
+    defaultValues: { email: '', password: '' },
+    resolver: zodResolver(loginSchema)
+  });
+
+  const onSubmit: SubmitHandler<LoginInput> = async (data) => {
+    // sent credentials to backend server
+    try {
+      const res = await axios.post('/auth/login', data);
+      toast.success('logged in successfully');
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        toast.error(err.response?.data.message);
+        return;
+      }
+      toast.error('something went wrong. try again later.');
+    }
+  };
+
   return (
     <div>
-      <form className="grid gap-6">
-        <div>
+      <form className="grid gap-6" onSubmit={handleSubmit(onSubmit)}>
+        <div className={errors.email ? 'text-red-500' : undefined}>
           <label htmlFor="">Email</label>
-          <input type="text" className="border px-3 py-1.5 w-full" />
+          <input
+            type="text"
+            className="border px-3 py-1.5 w-full"
+            {...register('email')}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
         </div>
 
-        <div>
+        <div className={errors.password ? 'text-red-500' : undefined}>
           <label htmlFor="">Password</label>
-          <input type="password" className="border px-3 py-1.5 w-full" />
+          <input
+            type="password"
+            className="border px-3 py-1.5 w-full"
+            {...register('password')}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
         </div>
 
         <div>
