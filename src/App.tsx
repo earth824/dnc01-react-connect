26c0130e94f317deda28baaefb3e23465673bdf6
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-02 17:18:57 +0700

    complete only access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { Toaster } from 'sonner';
+import { useEffect, useState } from 'react';
+import { axios } from './config/axios';
+import { userSchema } from './types/user.type';
+import { useAuthStore } from './store/useAuthStore';
 
 function App() {
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        const res = await axios.get('/auth/me');
+        const user = userSchema.parse(res.data.user);
+        setAuth(user);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    initAuth();
+  }, []);
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <>
       <RouterProvider router={router} />
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,15 @@
 import { Link } from 'react-router';
 import { useAuthStore } from '../store/useAuthStore';
+import { axios } from '../config/axios';
 
 export default function Header() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+
+  const handleClickLogout = async () => {
+    await axios.post('/auth/logout');
+    clearAuth();
+  };
 
   return (
     <header className="bg-amber-400 px-4 py-2 flex justify-between items-center">
@@ -11,7 +18,7 @@ export default function Header() {
         {isAuthenticated ? (
           <>
             <Link to="/">Home</Link>
-            <button>Logout</button>
+            <button onClick={handleClickLogout}>Logout</button>
             <Link to="/profile">Profile</Link>
           </>
         ) : (
