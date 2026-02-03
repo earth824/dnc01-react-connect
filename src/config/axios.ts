e49047ed0f0e6a73ca8a7d0218d6ca14a287040d
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-03 10:35:04 +0700

    add axios interceptor to handle access token

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import { useAuthStore } from '../store/useAuthStore';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true
+});
+
+instance.interceptors.request.use((config) => {
+  const accessToken = useAuthStore.getState().accessToken;
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
 });
 
 export { instance as axios };
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from 'react';
+import { axios } from '../config/axios';
+import { useAuthStore } from '../store/useAuthStore';
+
 export default function ProfilePage() {
+  const accessToken = useAuthStore((state) => state.accessToken);
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      const res = await axios.get('/auth/me');
+      console.log(res);
+    };
+    if (accessToken) fetchMe();
+  }, [accessToken]);
+
   return <div>ProfilePage</div>;
 }
